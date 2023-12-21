fe8e539fb229dbd7c34d869a1cff3276189342d2
Rakin Ishraq <holla@thatkidfrom.nyc>
2023-12-20 21:37:48 -0500

    fetch threads and posts

--- bot/bot.py
+++ bot/bot.py
@@ -1,19 +1,33 @@
-from config import *
+from config import *  # TOKEN, CHANNELS
 import discord
-from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-class Bot(commands.Bot):
-    async def on_ready(self):
-        print(f'We have logged in as {self.user}')
+def shorten(string, chars):
+    return string[:min(len(string), chars)]
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
+async def check_channels():
+    for channel_id in CHANNELS:
+        channel = client.get_channel(int(channel_id))
+        if channel is None:
+            print(f"Could not find channel {channel_id}")
+            continue
 
-        if str(message.channel.id) in CHANNELS:
-            print(f'Message from {message.author}: {message.content}')
+        if isinstance(channel, discord.ForumChannel):
+            for thread in channel.threads:
+                print("post:", channel.category, channel.name, thread.name)
+                async for message in thread.history(limit=1):
+                    print('\t', shorten(message, 25))
+        
+        elif isinstance(channel, discord.TextChannel):
+            for thread in channel.threads: # first pinned message in thread
+                print("thread:", channel.category, channel.name, thread.name)
 
-bot = Bot(command_prefix='!', intents=intents)
-bot.run(TOKEN)
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await check_channels()
+
+client.run(TOKEN)
