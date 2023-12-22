2d8bde14cb9f5dfeace4b8d4e1a67e958f4a6075
Rakin Ishraq <holla@thatkidfrom.nyc>
2023-12-22 10:37:58 -0500

    refactored webui to folder

---
+++ webui/app.py
@@ -0,0 +1,32 @@
+from bottle import run as run_web, route
+from bot.bot import concepts, sources
+from markdown import markdown
+
+@route('/')
+def index():
+    display = []
+    for c in concepts.values():
+        if c.id not in sources:
+            txt = c.category + ' :: ' + c.name
+            display.append(f"<a href=\"id/{c.id}\">{txt}</a>")
+    return '<br>'.join(display)
+
+
+@route('/id/<id:int>')
+def channel(id):
+    if id in concepts:
+        concept = concepts[id]
+        ret = {"parent": str(concept)}
+
+        if concept.post:
+            ret["post"] = markdown(concept.post)
+        if concept.source in concepts:
+            source = concepts[concept.source]
+            ret["source"] = str(source)
+        
+        return [f"------{k}------<br><br>{v}<br><br>" \
+                for (k,v) in ret.items()]
+
+
+def run_webui():
+    run_web(host='localhost', port=8080, quiet=True)
